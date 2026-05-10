6c042a5d07e3c4beeb575bbb17ad5eafba1354b5
prusse33 <252357@astanait.edu.kz>
2026-05-10 14:26:42 +0500

    init: graph traversal project

---
+++ src/Vertex.java
@@ -0,0 +1,19 @@
+public class Vertex {
+
+    private int id;
+
+    public Vertex(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{" +
+                "id=" + id +
+                '}';
+    }
+}
